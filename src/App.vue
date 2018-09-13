2055ff9ebd2ac465e1f113882d94425e010dae88
ororog <ororog@gmail.com>
2018-09-13 23:04:58 +0900

    add text.

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,34 @@
     <v-content>
       <v-container>
         <h1>牛耕式ビューワー</h1>
+        <v-btn>
+          吾輩は猫である
+        </v-btn>
+        <v-btn>
+          銀河鉄道の夜
+        </v-btn>
+        <v-btn>
+          Alice in Wonderland
+        </v-btn>
+      </v-container>
+      <v-container grid-list-md>
         <v-layout row wrap>
           <v-flex d-flex xs12 md6 class="box">
-            <v-textarea
-              auto-grow
-              outline
-              label="原文(テキストを編集できます)"
-              v-model="text">
-            </v-textarea>
+            <v-card>
+              <v-textarea
+                auto-grow
+                outline
+                hide-details
+                label="原文(テキストを編集できます)"
+                v-model="text">
+              </v-textarea>
+            </v-card>
           </v-flex>
           <v-flex d-flex xs12 md6 class="box">
-            <div class="textBoxWrap">
+            <v-card class="textBoxWrap">
               <div class="label">牛耕式</div>
-              <div ref="textBox" class="textBox">
-              </div>
-            </div>
+              <div ref="textBox" class="textBox"></div>
+            </v-card>
           </v-flex>
         </v-layout>
       </v-container>
@@ -28,6 +41,7 @@
 <script>
 import JQuery from 'jquery'
 import Neko from './wagahaiha_neko.txt'
+import Alice from './alice.txt'
 let $ = JQuery
 
 export default {
@@ -38,7 +52,7 @@ export default {
   },
   data () {
     return {
-      text: Neko
+      text: Alice
     }
   },
   methods: {
@@ -48,16 +62,16 @@ export default {
       let currentBox = $('<div></div>')
       textBox.append(currentBox)
       for (let i = 0; i < this.text.length; i++) {
+        console.log(this.text[i], this.text.charCodeAt(i))
         let height = currentBox.height()
-        if (height === 0) {
-          currentBox.text(currentBox.text() + this.text[i])
-        } else if (this.text[i] === '\n') {
+        if (this.text[i] === '\n') {
+          console.log('enter')
           currentBox.append('<br />')
           currentBox = $('<div></div>')
           textBox.append(currentBox)
         } else {
           currentBox.text(currentBox.text() + this.text[i])
-          if (currentBox.height() > height) {
+          if (height !== 0 && currentBox.height() > height) {
             currentBox.text(currentBox.text().slice(0, -1))
             currentBox = $('<div></div>')
             textBox.append(currentBox)
@@ -82,18 +96,20 @@ export default {
 .label {
     font-size: 12px;
 }
+
 .box {
     padding: 20px;
 }
+
 .textBoxWrap {
-    border-radius: 4px;
-    border: 2px solid black;
-    padding: 15px;
+    padding: 10px;
 }
+
 .textBox {
     font-size: 15px;
     line-height: 1.3;
 }
+
 .textBox div:nth-child(even) {
     transform: rotateY( 180deg );
 }
